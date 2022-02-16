2465f9bee2f1a5357a16ffe1d1cd6c6c9f6d7f50
Egor Evseenko <egorevseenko@inbox.ru>
2022-02-16 12:11:07 +0300

    fix bugs for ConsoleCommand class

--- TerminalWrapper2MacOS/TerminalWrapper2MacOS/ConsoleCommand.swift
+++ TerminalWrapper2MacOS/TerminalWrapper2MacOS/ConsoleCommand.swift
@@ -7,32 +7,43 @@
 
 import Foundation
 
+// class that execute shell/bash console commands
 class ConsoleCommand {
     // function that execute shell/bash commands with errors handling
     func safeWrapper(command: String, commandType: CommandType) throws -> String {
-        // Using the Process class, your program can run another program as a subprocess and can monitor that program’s execution
+        // Using the Process class, your program can run another program as a subprocess and can monitor that program’s execution.
         let task = Process()
-        // Pipe objects provide an object-oriented interface for accessing pipes.
         // An Pipe object represents both ends of a pipe and enables communication through the pipe.
         let pipe = Pipe()
+        // If file is an NSPipe object, launching the receiver automatically closes the write end of the pipe in the current task.
         
-        task.standardOutput = pipe // Sets the standard output for the receiver
-        task.standardError = pipe // Sets the standard error for the receiver
+        // Here we sets the standard output and error for the receiver
+        task.standardOutput = pipe
+        task.standardError = pipe
         // - c flag - Use Cscore processing of the scorefile
-        task.arguments = ["-c", command] // Sets the command arguments that should be used to launch the executable
-        task.executableURL = URL(fileURLWithPath: commandType.rawValue) // Sets filePath with selected (shell/bash) command
+        // Sets the command arguments that should be used to launch the executable
+        task.arguments = ["-c", command]
+        // Sets file URL of the receiver's executable file.
+        task.executableURL = URL(fileURLWithPath: commandType.rawValue)
         
+        // function call for run shell/bash command
         return try runTask(task: task, pipe: pipe)
     }
     
-    // run shell/bash command
+    // function for run shell/bash command
     private func runTask(task: Process, pipe: Pipe) throws -> String {
         do {
-            try task.run() // run shell/bash command
-        } catch { throw error }
+            // run selected command (shell/bash)
+            try task.run()
+        } catch {
+            // handle command execution error
+            throw error
+        }
         
+        // Reads the available data synchronously up to the end of file or maximum number of bytes.
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
         if let output = String(data: data, encoding: .utf8) {
+            // handle command data
             return output
         } else {
             // handle command execution error
